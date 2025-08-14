08cc8aa0dfd21428e587061ab0b66df3f08e642a
Asrafsaleh <asrafsalehmalik77@gmail.com>
2025-08-14 15:12:17 +0800

    Create 4.BIlangan ganjil genap.cpp

---
+++ Latihan/4.BIlangan ganjil genap.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+  int angka;
+
+  cout << "=== Program Menentukan Bilangan Ganjil/Genap ===" << endl;
+  cout << "Masukkan sebuah bilangan: ";
+  cin >> angka;
+
+ if (angka % 2 == 00) {
+   cout << angka << " adalah bilangan GENAP." << endl;
+ } 
+ else {
+    cout << angka << " adalah bilangan GANJIL." << endl;
+ }
+
+return 0;
+}
