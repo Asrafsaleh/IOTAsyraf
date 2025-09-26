854bc31858eeee585d9f40a7abc8f2206d080b1c
Asrafsaleh <asrafsalehmalik77@gmail.com>
2025-09-26 13:27:32 +0800

    Create 22.Rekursif faktorial.cpp

---
+++ Latihan/22.Rekursif faktorial.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int faktorial (int n) {
+   if (n == 0 || n == 1) {
+       return 1;
+   } else {
+         return n * faktorial(n - 1);
+   }
+}
+ int main  () {
+      int angka;
+      cout << "Masukkan Bilangan: "; 
+       cin >> angka;
+ if (angka < 0) {
+       cout << "Faktorial tidak terdefinisi untuk bilangan negatif."
+         } else {
+              cout << "Faktorial dari" << angka << "adalah: " << faktorial(angka) << endl;
+ }
+return 0;
+ }
+  
